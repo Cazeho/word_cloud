2daefbafb73f8cd2e0602d02f6833d82d7ab64f7
Cazeho <58745332+Cazeho@users.noreply.github.com>
2021-04-26 10:47:14 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,115 +2,106 @@
 #include <string>
 #include <list>
 #include "dirfilelist.h"
-#include<fstream>
-#include<stdio.h>
+#include <fstream>
+#include <stdio.h>
 #include <windows.h>
 #include <cstdio>
+#include <cstdlib>
+#include <map>
 #include <sstream>
-#include <string>
+
+
+
+
+
 
 using namespace std;
+
+
+
+using namespace std;
+void wordsCount(string str) {
+	int count = 1;
+	//key value pair map
+	//key = word, value = count
+	std::map<string, int> wordsMap;
+	//extract words and push in map
+	istringstream wordStream(str);
+	string word;
+
+	//Extract each words and add to map
+	//if it does not exist in map
+	while (wordStream >> word) {
+		//create a pair of map and bool
+		pair<map<string, int>::iterator, bool> retrunValue;
+		//insert element
+		retrunValue = wordsMap.insert(pair<string, int>(word, count));
+		//Check if map contains value
+		if (retrunValue.second == false)
+		{
+			//Element already exists
+			++retrunValue.first->second;
+		}
+
+	}//while
+	 //Iterate elements and print words
+	map<string, int>::iterator itr;
+	for (itr = wordsMap.begin(); itr != wordsMap.end(); ++itr) {
+		cout << '\t' << itr->first;
+		cout << '\t' << itr->second << '\n';
+	}
+
+}
 
 int main()
 {
-  string dirName;
+	string dirName;
 
-  cout<<"Enter directory name: ";
-  cin>>dirName;
+	cout << "Enter directory name: ";
+	cin >> dirName;
 
-  list<string> fileList;
-  bool error=getDirectoryFileList(dirName,fileList);
+	list<string> fileList;
+	bool error = getDirectoryFileList(dirName, fileList);
 
-  if (error) cout<<"An error occured"<<endl;
-  else
-  {
-    fileList.sort();
-    cout << "Number of files: " << fileList.size() << endl;
-    cout << "File list: " << endl;
-    for (list<string>::iterator it = fileList.begin(); it != fileList.end(); it++)
-      cout << *it << endl;
-  }
-
-  
-  string f;
-
-  system("pause");
-  cout << dirName;
-  cout << "\n";
-  cout << "select a file :";
-  cin>> f;
-  dirName = dirName + "\\" + f;
-  cout << "\n";
-  cout << dirName;
+	if (error) cout << "An error occured" << endl;
+	else
+	{
+		fileList.sort();
+		cout << "Number of files: " << fileList.size() << endl;
+		cout << "File list: " << endl;
+		for (list<string>::iterator it = fileList.begin(); it != fileList.end(); it++)
+			cout << *it << endl;
+	}
 
 
+	string f;
 
-  string myText;
+	system("pause");
+	cout << dirName;
+	cout << "\n";
+	cout << "select a file :";
+	cin >> f;
+	dirName = dirName + "\\" + f;
+	cout << "\n";
+	cout << dirName;
+	
+	
+	
 
+	string combine;
+	string input;
 
-  ifstream MyReadFile(dirName);
+	ifstream dataFile(dirName);
 
-  cout << "\n";
-  while (getline(MyReadFile, myText)) {
+	if (dataFile)
+		while (getline(dataFile, input))
+			combine += input;
 
-	  cout << myText;
-  }
-  cout << "\n";
-      
-      
-  return 0;
-}
+	cout << combine << endl;
+	wordsCount(combine);
 
+	
+	
 
-/*
-#include <map>
-#include<sstream>
-#include<iostream>
-
-using namespace std;
-
-void wordsCount(string str){
-
-      int count = 1;
-      //key value pair map
-      //key = word, value = count
-      std::map<string,int> wordsMap;
-
-      //extract words and push in map
-      istringstream wordStream(str);
-
-      string word;
-      //Extract each words and add to map
-      //if it does not exist in map
-       while(wordStream >> word){
-             //create a pair of map and bool
-             pair<map<string,int>::iterator,bool> retrunValue;
-
-             //insert element
-            retrunValue = wordsMap.insert( pair<string,int>(word,count));
-
-            //Check if map contains value
-            if (retrunValue.second==false)
-            {
-                  //Element already exists
-                  ++retrunValue.first->second; 
-            }     
-       }//while
-
-       //Iterate elements and print words 
-       map<string,int>::iterator itr ;
-
-       for (itr = wordsMap.begin(); itr != wordsMap.end(); ++itr){
-
-               cout << '\t' << itr->first ;
-               cout<< '\t' << itr->second << '\n';
-       }
-}
-
-int main(){
-
-      wordsCount("hello world !!! hello world !!! good day");    
-
-      return 0;
-
-}*/
+	return 0;
+};
