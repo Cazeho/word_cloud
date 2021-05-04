8b7f502643a4e8f6d548a6b0269accfdbbd8d158
Cazeho <58745332+Cazeho@users.noreply.github.com>
2021-05-04 11:19:44 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -122,6 +122,40 @@ int main()
 
 using namespace std;
 
+
+string removeWord(string str, string word)
+{
+	// Check if the word is present in string
+	// If found, remove it using removeAll()
+	if (str.find(word) != string::npos)
+	{
+		size_t p = -1;
+
+		// To cover the case
+		// if the word is at the
+		// beginning of the string
+		// or anywhere in the middle
+		string tempWord = word + " ";
+		while ((p = str.find(word)) != string::npos)
+			str.replace(p, tempWord.length(), "");
+
+		// To cover the edge case
+		// if the word is at the
+		// end of the string
+		tempWord = " " + word;
+		while ((p = str.find(word)) != string::npos)
+			str.replace(p, tempWord.length(), "");
+	}
+
+	// Return the resultant string
+	return str;
+}
+
+
+
+
+
+
 template <class T>
 struct less_second : binary_function<T, T, bool>
 {
@@ -233,7 +267,7 @@ int main()
 	t.lireFile(dirName);
 
 	
-
+	cout <<removeWord(t.lireFile(dirName), choix)<< endl;
 
 	return 0;
 }
