2ef78fd25ce5af427c456518b88df31e9cdce950
Cazeho <58745332+Cazeho@users.noreply.github.com>
2021-05-04 10:14:24 +0200

    Update text.h

--- text.h
+++ text.h
@@ -1,1 +1,18 @@
 
+#pragma once
+#include <string>
+using namespace std;
+
+class text {
+
+	public:
+		void directory();
+		void WordCloud();
+		void afficheWordCloud();
+		void modifier_parameterWorCloud();
+
+		void ecrireFile(string path,char *values);
+		
+
+
+};
