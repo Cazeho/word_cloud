97b555955239a954ac1816932e33c4795ee9a663
Cazeho <58745332+Cazeho@users.noreply.github.com>
2021-05-10 14:56:50 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -112,3 +112,173 @@ int main()
 	cout << t.removeWord(t.lireFile(dirName), choix) << endl;
 	return 0;
 }
+
+
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+#include <map>
+#include <iostream>
+#include <string>
+#include <list>
+#include <fstream>
+#include <stdio.h>
+#include <windows.h>
+#include <cstdio>
+#include <cstdlib>
+#include <map>
+#include <sstream>
+
+#include "text.h"
+
+
+
+#include <string>
+#include <vector>
+using namespace std;
+
+
+
+string removeDupWord(string str)
+{
+	// Used to split string around spaces.
+	istringstream ss(str);
+
+	string word; // for storing each word
+
+				 // Traverse through all words
+				 // while loop till we get 
+				 // strings to store in string word
+	while (ss >> word)
+	{
+		// print the read word
+		 cout << word << "\n";
+		 return word;
+	}
+}
+
+
+
+
+template <class T>
+struct less_second : binary_function<T, T, bool>
+{
+	inline bool operator()(const T& lhs, const T& rhs)
+	{
+		return lhs.second.value < rhs.second.value;
+	}
+};
+class WordCounter
+{
+public:
+	int value;
+	WordCounter() : value(0) {}
+	void operator++ (int) { value++; }
+};
+ostream& operator<<(ostream& st, WordCounter& wc)
+{
+	return st << wc.value;
+}
+// Remove unwanted characters from a string  
+bool filter(char c)
+{
+	return isalpha(c) == 0;
+}
+const string path = "C:\\Users\\manuel\\Desktop\\C++\\test.txt";
+
+
+
+
+
+
+
+
+
+int main()
+{
+	typedef pair< string, WordCounter > word_mapping;
+	map<string, WordCounter> counter;
+	ifstream input;
+	input.open(path.c_str());
+	if (!input)
+	{
+		cout << "Error in opening file\n";
+		return 0;
+	}
+	string tok;
+	while (true)
+	{
+		input >> tok;
+		// Remove ?, !, , characters etc from string  
+		tok.resize(remove_if(tok.begin(), tok.end(), filter) - tok.begin());
+		if (input)
+		{
+			counter[tok]++;
+		}
+		else break;
+	}
+	map< string, WordCounter, less<string> >::iterator it = counter.begin();
+	// And then sort and display the result:
+	vector< word_mapping > result(counter.begin(), counter.end());
+	sort(result.rbegin(), result.rend(), less_second<word_mapping>());
+	for (vector< word_mapping >::const_iterator mit = result.begin();
+		mit != result.end();
+		mit++)
+	{
+		cout << setw(15)
+			<< (*mit).first
+			<< "\t"
+			<< (*mit).second.value
+			<< endl;
+	}
+	ofstream outfile("C:\\Users\\manuel\\Desktop\\C++\\process.txt");
+	for (vector< word_mapping >::const_iterator mit = result.begin();
+		mit != result.end();
+		mit++)
+	{
+		outfile << (*mit).first << endl;
+	}
+	outfile.close();
+	string choix;
+	system("pause");
+	cout << "choisir: ";
+	cin >> choix;
+	string dirName;
+	dirName = "C:\\Users\\manuel\\Desktop\\C++\\process.txt";
+
+
+	text t;
+	 t.lireFile(dirName);
+
+	cout << t.lireFile(dirName) << endl;
+	cout << "----" << endl;
+
+	cout << t.removeWord(t.lireFile(dirName), choix) << endl;
+
+
+
+
+//////////////////////////////////////////////////////
+
+	removeDupWord(t.removeWord(t.lireFile(dirName), choix));
+	
+
+
+
+
+
+	int n = 10;
+	string tab[n] = {"hello","salut"};
+	ofstream outfile1("C:\\Users\\manuel\\Desktop\\C++\\process2.txt");
+	for (int i=0;i<n;i++)
+	{
+		outfile1 << removeDupWord(t.removeWord(t.lireFile(dirName), choix)) << endl;
+	}
+	outfile.close();
+
+
+	return 0;
+}
